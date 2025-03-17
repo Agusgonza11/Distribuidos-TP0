63198edb26577e9bf54e93d3a87ded6554895f9e
Agustin Gonzalez <aguselgonza@gmail.com>
2025-03-16 21:38:26 -0300

    correcciones para test

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -7,7 +7,7 @@ MESSAGE="Hola mundo"
 RESPONSE=$(docker run --rm --network="$NETWORK_NAME" busybox sh -c "echo '$MESSAGE' | nc $SERVER_NAME $SERVER_PORT")
 echo "$RESPONSE"
 
-if [[ "$RESPONSE" == "$MESSAGE" ]]; then
+if [ "$RESPONSE" = "$MESSAGE" ]; then
     echo "action: test_echo_server | result: success"
 else
     echo "action: test_echo_server | result: fail"
