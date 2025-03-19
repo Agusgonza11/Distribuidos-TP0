d5e6301529054bfee8fc726bf3a2e83c54a2551f
Agustin Gonzalez <aguselgonza@gmail.com>
2025-03-18 21:30:49 -0300

    constantes para datos de clientes

--- mi-generador.py
+++ mi-generador.py
@@ -1,8 +1,15 @@
 import sys 
 import yaml
 
+def cargar_clientes_desde_archivo(archivo_clientes):
+    """Cargo clientes desde archivo YAML"""
+    with open(archivo_clientes, "r") as file:
+        data = yaml.safe_load(file)
+    return data.get("clientes", [])
 
-def generar_yaml(cantidad_clientes):
+
+def generar_yaml(cantidad_clientes, clientes):
+    """Genera un docker-compose.yaml"""
     yaml = {
         "name": "tp0",
         "services": {
@@ -30,12 +37,18 @@ def generar_yaml(cantidad_clientes):
     }
 
     for i in range(1, int(cantidad_clientes) + 1):
+        cliente = clientes[i - 1] if i - 1 < len(clientes) else {}
         yaml["services"][f"client{i}"] = {
             "container_name": f"client{i}",
             "image": "client:latest",
             "entrypoint": "/client",
             "environment": [
                 f"CLI_ID={i}",
+                f"NOMBRE={cliente.get('NOMBRE', 'Unknown')}",
+                f"APELLIDO={cliente.get('APELLIDO', 'Unknown')}",
+                f"DOCUMENTO={cliente.get('DOCUMENTO', '00000000')}",
+                f"NACIMIENTO={cliente.get('NACIMIENTO', '1900-01-01')}",
+                f"NUMERO={cliente.get('NUMERO', '0000')}",
             ],
             "networks": ["testing_net"],
             "depends_on": ["server"],
@@ -48,13 +61,14 @@ def generar_yaml(cantidad_clientes):
    
 
 
-def generar_docker_compose(nombre_archivo, cantidad_clientes):
+def generar_docker_compose(nombre_archivo, cantidad_clientes, archivo_clientes):
+    clientes = cargar_clientes_desde_archivo(archivo_clientes)
     with open(nombre_archivo, "w") as archivo:
-        yaml.dump(generar_yaml(cantidad_clientes), archivo)
+        yaml.dump(generar_yaml(cantidad_clientes, clientes), archivo)
 
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         sys.exit(1)
-    generar_docker_compose(sys.argv[1], sys.argv[2])
+    generar_docker_compose(sys.argv[1], sys.argv[2], sys.argv[3])
