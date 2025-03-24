d8eba96a14a1bdf41c20a35cd64b632abace220e
Agustin Gonzalez <aguselgonza@gmail.com>
2025-03-24 00:03:12 -0300

    fix

--- mi-generador.py
+++ mi-generador.py
@@ -22,9 +22,8 @@ def generar_yaml(cantidad_clientes, clientes):
                 ],
                 "networks": ["testing_net"],
                 "volumes": [
-                    "./server/config.ini:/config.ini"
-                    "./bets.csv:/bets.csv"
-
+                    "./server/config.ini:/config.ini",
+                    # "./bets.csv:/bets.csv" Descomentar para debuggear
                 ]
             }
         },
